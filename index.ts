58d20a3387da9a0c1637a635361c12b6a6d6ba85
Abu Huraira Arain <arainhuraira@gmail.com>
2024-04-02 00:39:10 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,64 @@
+import inquirer from "inquirer";
+
+let myBalance = 20000;
+const myPin = 1999;
+
+let pinEntered = await inquirer.prompt([
+    {
+        name: "pin",
+        type: "number",
+        message: "Enter your pin code",
+    },
+]);
+
+if (pinEntered.pin === myPin) {
+    let atmQuestion = await inquirer.prompt([
+        {
+            name: "accountType",
+            type: "list",
+            message: "Select your account type",
+            choices: ["Current Account", "Saving Account"],
+        },
+        {
+            name: "transMethod",
+            type: "list",
+            message: "Select your transaction method",
+            choices: ["Fast cash", "Withdrawal cash"],
+        },
+    ]);
+    if (atmQuestion.transMethod === "Withdrawal cash") {
+        let cashWithdrawalCash = await inquirer.prompt([
+            {
+                name: "withdraw",
+                type: "number",
+                message: "Enter your amount",
+            },
+        ]);
+        if (myBalance >= cashWithdrawalCash.withdraw) {
+            myBalance -= cashWithdrawalCash.withdraw;
+            console.log(`Your remaning balance is :${myBalance}`);
+        } else {
+            console.log("Insufficient balance");
+        }
+    } else {
+        let fastCashAmount = await inquirer.prompt([
+            {
+                name: "fastCash",
+                type: "list",
+                message: "Select the amount",
+                choices: ["1000", "3000", "5000", "10000"]
+            }
+        ]);
+        if (myBalance >= fastCashAmount.fastCash) {
+            myBalance -= fastCashAmount.fastCash
+            console.log(`Your balance is :${myBalance}`);
+
+        } else {
+            console.log("Insufficient balance");
+
+        }
+    }
+} else {
+    console.log("Incorrect pin code");
+
+}
